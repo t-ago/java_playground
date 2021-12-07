51d463e1095a58bfc8cf5096ec72c5abae767ba0
t-ago <t.ago.mm1@gmail.com>
2021-12-07 16:23:48 -0300

    Fix mean calculation in GradesService

--- swing/src/Service/GradesService.java
+++ swing/src/Service/GradesService.java
@@ -1,25 +1,30 @@
 package Service;
 
 public class GradesService {
-    private Integer[] grades = { 0, 0, 0, 0, 0 };
+    private Integer[] votesCount = { 0, 0, 0, 0, 0 };
 
     public String getAverage() {
-        double total = 0;
+        double total = 0.0;
+        int votesRegistered = 0;
 
-        for (int i = 0; i < grades.length; i++) {
-            total += (grades[i] * (i + 1));
+        for (int i = 0; i < votesCount.length; i++) {
+            votesRegistered += votesCount[i];
+            total += votesCount[i] * (i + 1);
         }
 
-        total = total / (double) grades.length;
+        if(total != 0){
+            total /= votesRegistered;
+        }
 
+        Math.round(total);
         return String.format("%.1f", total);
     }
 
     public void incrementOn(Integer index) {
-        grades[index]++;
+        votesCount[index]++;
     }
 
     public Integer[] getGradesList() {
-        return grades;
+        return votesCount;
     }
 }
